2235058f2a654b6831e80f5fca8acf3dbb0a6660
takashialpha <takashialpha@protonmail.com>
2026-01-02 18:46:09 -0300

    changed phonic to rodio, added ratatui and set up for start

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,13 @@
+use crate::cli::Cli;
 use app_base::{
     App,
     app::{Context, Privilege, error::AppError},
 };
 
-use crate::cli::Cli;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Default, Serialize, Deserialize, Clone)]
-pub struct AppConfig;
-
 pub struct MotMot;
 
 impl App for MotMot {
-    type Config = AppConfig;
+    type Config = ();
     type Cli = Cli;
 
     fn privilege() -> Privilege {
@@ -21,6 +16,6 @@ impl App for MotMot {
 
     fn run(&self, ctx: Context<Self::Config, Self::Cli>) -> Result<(), AppError> {
         println!("{:?}", ctx);
-        todo!()
+        Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ pub const APP_NAME: &str = "audium";
 
 pub mod app;
 pub mod cli;
+pub mod player;
+pub mod tui;
